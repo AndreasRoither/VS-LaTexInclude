58984da71f717f48dd6a82a7067d3f7cb88501a3
AndiRoither <andi.roither@hotmail.de>
2016-11-29 14:10:47 +0100

    New Tex Classes
    - Add
    TexTemplate.cs
    TexTemplate_PDF.cs
    TexTemplate_Code.cs
    TexTemplate_Picture.cs
    
    Code, Picture, PDF all inherit from TexTemplate + they have their own
    necessary variables.
    
    -----------
    var tex= new TexTemplate_Code(....)
    {
    Language= "Pascal",
    Path = "../.."
    };
    
    Console.WriteLine("{Language} and Path is {Path }".FormatWith(tex));
    
    This would be a Solution to the Text Templates.

---
+++ LatechInclude/HelperClasses/TexTemplate_PDF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatechInclude.HelperClasses
+{
+    class TexTemplate_PDF : TexTemplate
+    {
+        public TexTemplate_PDF(string Type, string Path) : base(Type, Path)
+        {
+            this.Type = Type;
+            this.Path = Path;
+        }
+    }
+}
